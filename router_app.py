21ad5e57ab9497c0b0785d2d5372806765dd8fc0
twiemold <twiemold@uvm.edu>
2022-05-05 17:44:01 -0400

    basic query_network working

--- router_app.py
+++ router_app.py
@@ -15,6 +15,14 @@ http://ceskalka.w3.uvm.edu/265/sdn-assignment/
 
 """
 
+import requests as req
+import sys
+import json
+
+TEST_URL = "http://www.goatgoose.com"
+PLAIDNET_URL = "https://132.198.11.11"
+SOCKET = ":2222"
+
 
 def deploy_tables():
     # deploy the forwarding tables to the network via the controller
@@ -28,9 +36,17 @@ def comp_tables():
 
 
 def query_network():
-    # query the network controller for its topology (RESTfully)
-    pass
+    # TODO: Add error handling
+    # TODO: Parameterize arguments
+    topo_link = '/get_topology/topology1'
+    response = req.get(TEST_URL + SOCKET + topo_link, timeout=10)
+    json_topo = response.json()
+    return json_topo
+
+
+def main():
+    topo = query_network()
 
 
 if __name__ == '__main__':
-    query_network()
+    main()
